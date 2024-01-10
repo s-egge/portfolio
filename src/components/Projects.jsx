fb2b387f9007ec49952764b5073b19821b13737e
s-egge <ssteele836@gmail.com>
2024-01-10 10:51:00 -0800

    Projects progress

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,8 +4,8 @@ import './css/projects.css';
 function ProjectCard({title, description, img, projectLink, githubLink}) {
   return (
     <article className="project-card">
-      <h3>{title}</h3>
       <img src={img} alt={title + " photo"} />
+      <h3>{title}</h3>
       <p>{description}</p>
       <a href={projectLink}>View Project</a>
       <a href={githubLink}>View Code</a>
@@ -16,10 +16,33 @@ function ProjectCard({title, description, img, projectLink, githubLink}) {
 function Projects() {
   //variable that holds set of projects, each has title, description, and link
   const projects = [
-    {title: "Project 1", description: "This is a description of project 1", img: "src/assets/peasel.png", projectLink: "https://www.google.com", githubLink: "https://www.google.com"},
-    {title: "Project 2", description: "This is a description of project 2", img: "src/assets/peasel.png", projectLink: "https://www.google.com", projectLink: "https://www.google.com", githubLink: "https://www.google.com"},
-    {title: "Project 3", description: "This is a description of project 3", img: "src/assets/peasel.png", projectLink: "https://www.google.com", projectLink: "https://www.google.com", githubLink: "https://www.google.com"},
-    {title: "Project 4", description: "This is a description of project 4", img: "src/assets/peasel.png", projectLink: "https://www.google.com", projectLink: "https://www.google.com", githubLink: "https://www.google.com"},
+    {
+      title: "Peasel", 
+      description: "A browser tool for creating pixel art", 
+      img: "src/assets/peasel.png", 
+      projectLink: "https://peasel.onrender.com/", 
+      githubLink: "https://github.com/s-egge/peasel"
+    },
+    {
+      title: "Animal Shelter Database", 
+      description: "A webpage for managing data for a fictional animal shelter with an SQL database", 
+      img: "src/assets/animal-database.png", 
+      projectLink: "146.235.204.101:27469", 
+      githubLink: "https://github.com/s-egge/animal-shelter-database"
+    },
+    {
+      title: "Picnic Defender", 
+      description: "A group project for an intro Web Developement class, this is a game where the player must defend their picnic against enroaching bugs", 
+      img: "src/assets/picnic-defender.png", 
+      projectLink: "https://www.google.com", 
+      githubLink: "https://www.google.com"},
+    {
+      title: "Portfolio", 
+      description: "This web page! A desktop and mobile friendly page where I showcase my projects", 
+      img: "src/assets/portfolio.png", 
+      projectLink: "localhost:5173/",
+      githubLink: "https://github.com/s-egge/portfolio"
+    },
   ]
 
     return (
