be819db40227fd738ed2c3964b5f3c5073448fd2
s-egge <ssteele836@gmail.com>
2024-01-12 15:36:34 -0800

    Projects updates
    - Videos play on hover on image
    - Tech added for each project
    - CSS tweaks

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
-import {useState}  from 'react';
-import HoverVideoPlayer from 'react-hover-video-player';
+import {useState, useEffect}  from 'react';
+import HoverVideoPlayer from 'react-hover-video-player'; 
 import './css/projects.css';
 
 
@@ -30,13 +30,20 @@ function ImgVideoComponent({video, img, alt}) {
 }
 
 
-function ProjectCard({title, description, img, video, alt, projectLink, githubLink}) {
+function ProjectCard({title, description, img, video, alt, tech, projectLink, githubLink}) {
   return (
     <article className="project-card">
       <ImgVideoComponent video={video} img={img} alt={alt} />
       <div className="project-info">
-        <h2>{title}</h2>
-        <p>{description}</p>
+        <div className="title-description-wrapper">
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+        <div className="tech-wrapper">
+          {tech.map((tech, index) => (
+            <p key={index}>{tech}</p>
+          ))}
+        </div>
         <div className="project-links">
           {projectLink !== '' && <a href={projectLink}>View Project</a>}
           <a href={githubLink}>View Code</a>
@@ -55,6 +62,7 @@ function Projects() {
       img: "src/assets/peasel.png",
       video: "src/assets/peasel.mp4",
       alt: "peasel website photo/video",
+      tech: ["html", "css", "javascript", "express"],
       projectLink: "https://peasel.onrender.com/", 
       githubLink: "https://github.com/s-egge/peasel"
     },
@@ -64,6 +72,7 @@ function Projects() {
       img: "src/assets/animal-database.png", 
       video: "src/assets/animal-database.mp4",
       alt: "animal shelter database website photo/video",
+      tech: ["html", "css", "javascript", "express", "sql", "mysql", "sqlite"],
       projectLink: "", 
       githubLink: "https://github.com/s-egge/animal-shelter-database"
     },
@@ -73,23 +82,44 @@ function Projects() {
       img: "src/assets/picnic-defender.png", 
       video: "src/assets/picnic-defender.mp4",
       alt: "picnic defender website photo/video",
+      tech: ["html", "css", "javascript", "express"],
       projectLink: "https://www.google.com", 
       githubLink: "https://www.google.com"},
     {
       title: "Portfolio", 
       description: "This web page! A desktop and mobile friendly page where I showcase my projects", 
       img: "src/assets/portfolio.png", 
+      video: "src/assets/portfolio.mp4",
+      alt: "portfolio website photo/video",
+      tech: ["html", "css", "javascript", "react"],
       projectLink: "localhost:5173/",
       githubLink: "https://github.com/s-egge/portfolio"
     },
   ]
+
+  const [maxHeight, setMaxHeight] = useState(0);
+
+  useEffect(() => {
+    //get height of largest card after they render
+    const projectCards = document.querySelectorAll('.project-card');
+    let maxCardHeight = 0;
+
+    projectCards.forEach((card) => {
+      const cardHeight = card.offsetHeight;
+      if (cardHeight > maxCardHeight) {
+        maxCardHeight = cardHeight;
+      }
+    });
+
+    setMaxHeight(maxCardHeight);
+  }, []);
 
     return (
       <div className="projects-container">
         <h3>Projects</h3>
         <div className="projects-wrapper">
           {projects.map((project, index) => (
-            <ProjectCard key={index} {...project} />
+            <ProjectCard key={index} {...project} style={{ height: `${maxHeight}px` }} />
           ))}
         </div>
       </div>
