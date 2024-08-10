0c88890e22697c2aa218d47ac1dff0f6dd9f9a77
s-egge <ssteele836@gmail.com>
2024-08-10 08:46:32 -0700

    Added a project
    Added Book Keeper project

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -1,30 +1,50 @@
 export const projectData = [
   {
-    title: "Youtube Tidy",
-    description: "A group project for Advanced Web Development, this project allows users to manage their YouTube playlists and subscriptions by bulk moving, adding, or deleting videos. You can sign in with a valid ONID account or checkout the website as a guest user",
-    mediaName: "youtube-tidy",
-    alt: "youtube tidy website photo/video",
-    tech: ["html", "css", "typescript", "react", "redux", "google oauth", "youtube api"],
-    projectLink: "https://youtube-tidy.netlify.app/",
-    githubLink: "https://github.com/s-egge/youtube-tidy"
+    title: "Book Keeper",
+    description:
+      "A group project for Mobile Web Development, an Android app for tracking book reading progress, taking and searching notes.",
+    mediaName: "book-keeper",
+    alt: "bookkeeper app photo/video",
+    tech: ["kotlin", "openlibrary api", "room database", "coroutines"],
+    projectLink: "",
+    githubLink: "https://github.com/s-egge/book-keeper",
   },
   {
-    title: "Peasel", 
-    description: "A browser tool for creating pixel art", 
+    title: "YouTube Tidy",
+    description:
+      "A group project for Advanced Web Development, this project allows users to manage their YouTube playlists and subscriptions by bulk moving, adding, or deleting videos. You can sign in with a valid ONID account or checkout the website as a guest user",
+    mediaName: "youtube-tidy",
+    alt: "youtube tidy website photo/video",
+    tech: [
+      "html",
+      "css",
+      "typescript",
+      "react",
+      "redux",
+      "google oauth",
+      "youtube api",
+    ],
+    projectLink: "https://youtube-tidy.netlify.app/",
+    githubLink: "https://github.com/s-egge/youtube-tidy",
+  },
+  {
+    title: "Peasel",
+    description: "A browser tool for creating pixel art",
     mediaName: "peasel",
     alt: "peasel website photo/video",
     tech: ["html", "css", "javascript", "express"],
-    projectLink: "https://peasel.onrender.com/", 
-    githubLink: "https://github.com/s-egge/peasel"
+    projectLink: "https://peasel.onrender.com/",
+    githubLink: "https://github.com/s-egge/peasel",
   },
   {
-    title: "Animal Shelter Database", 
-    description: "A webpage for managing data for a fictional animal shelter with an SQL database", 
+    title: "Animal Shelter Database",
+    description:
+      "A webpage for managing data for a fictional animal shelter with an SQL database",
     mediaName: "animal-database",
     alt: "animal shelter database website photo/video",
     tech: ["html", "css", "javascript", "express", "sql", "mysql", "sqlite"],
-    projectLink: "", 
-    githubLink: "https://github.com/s-egge/animal-shelter-database"
+    projectLink: "",
+    githubLink: "https://github.com/s-egge/animal-shelter-database",
   },
   {
     title: "Campfire Scene",
@@ -33,24 +53,26 @@ export const projectData = [
     alt: "campfire scene photo/video",
     tech: ["c++", "glsl", "opengl"],
     projectLink: "",
-    githubLink: "https://github.com/s-egge/campfire-scene"
+    githubLink: "https://github.com/s-egge/campfire-scene",
   },
   {
-    title: "Portfolio", 
-    description: "This web page! A desktop and mobile friendly page where I showcase my projects", 
+    title: "Portfolio",
+    description:
+      "This web page! A desktop and mobile friendly page where I showcase my projects",
     mediaName: "portfolio",
     alt: "portfolio website photo/video",
     tech: ["html", "css", "javascript", "react"],
     projectLink: "https://samanthaegge.com/",
-    githubLink: "https://github.com/s-egge/portfolio"
+    githubLink: "https://github.com/s-egge/portfolio",
   },
   {
-    title: "Picnic Defender", 
-    description: "A group project for an intro Web Developement class, this is a game where the player must defend their picnic against enroaching bugs", 
+    title: "Picnic Defender",
+    description:
+      "A group project for an intro Web Developement class, this is a game where the player must defend their picnic against enroaching bugs",
     mediaName: "picnic-defender",
     alt: "picnic defender website photo/video",
     tech: ["html", "css", "javascript", "express"],
-    projectLink: "https://www.google.com", 
-    githubLink: "https://www.google.com"
+    projectLink: "https://www.google.com",
+    githubLink: "https://www.google.com",
   },
 ];
