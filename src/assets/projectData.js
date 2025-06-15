fab8001348af0b566d946641b3814947a0501cef
s-egge <ssteele836@gmail.com>
2025-06-15 11:34:42 -0700

    Add new projects

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -1,5 +1,33 @@
 export const projectData = [
   {
+    title: "Ephira",
+    description:
+      "My senior capstone project with three other students, Ephira is a mobile app published for both iOS and Android. Ephira is a menstruation tracking app that keeps data local only and enrypted on the user's device. It features a calendar view, a symptom tracker, and a period prediction algorithm. The app is designed to be user-friendly, customizable, and accessible, with a focus on privacy and security.",
+    alt: "ephira app photo/video",
+    mediaName: "ephira",
+    tech: [
+      "react native",
+      "typescript",
+      "expo",
+      "zustand",
+      "sqlite",
+      "drizzle",
+      "react native paper",
+    ],
+    projectLink: "https://ephira.capucity.be/",
+    githubLink: "https://github.com/adulbrich/ephira",
+  },
+  {
+    title: "Quilters Canvas",
+    description:
+      "A web application for quilters to design their hexagon and square patterned quilts using images of fabric or solid colors. Users can create patterns and then save/load them both in the browser or locally. Both the size of the quilt grid and the scale of the fabric images can be adjusted to better reflect what the finished quilt will look like.",
+    mediaName: "quilters-canvas",
+    alt: "quilters canvas website photo/video",
+    tech: ["html", "css", "typescript", "react", "redux", "mantine"],
+    projectLink: "https://quilterscanvas.com/",
+    githubLink: "https://github.com/s-egge/quilters-canvas",
+  },
+  {
     title: "Book Keeper",
     description:
       "A group project for a Mobile Web Development class. Book Keeper is an Android app for tracking book reading progress, taking and searching notes.",
@@ -26,6 +54,16 @@ export const projectData = [
     ],
     projectLink: "https://youtube-tidy.netlify.app/",
     githubLink: "https://github.com/s-egge/youtube-tidy",
+  },
+  {
+    title: "Portfolio",
+    description:
+      "This web page! A desktop and mobile friendly page where I showcase my projects. It's seen a few iterations, so the video may not match the current design.",
+    mediaName: "portfolio",
+    alt: "portfolio website photo/video",
+    tech: ["html", "css", "javascript", "react"],
+    projectLink: "https://samanthaegge.com/",
+    githubLink: "https://github.com/s-egge/portfolio",
   },
   {
     title: "Peasel",
@@ -55,16 +93,7 @@ export const projectData = [
     projectLink: "",
     githubLink: "https://github.com/s-egge/campfire-scene",
   },
-  {
-    title: "Portfolio",
-    description:
-      "This web page! A desktop and mobile friendly page where I showcase my projects. It's seen a few iterations, so the video may not match the current design.",
-    mediaName: "portfolio",
-    alt: "portfolio website photo/video",
-    tech: ["html", "css", "javascript", "react"],
-    projectLink: "https://samanthaegge.com/",
-    githubLink: "https://github.com/s-egge/portfolio",
-  },
+
   {
     title: "Picnic Defender",
     description:
